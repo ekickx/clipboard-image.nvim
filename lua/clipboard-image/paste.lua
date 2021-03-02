de6e31973217eeca88bf84036abcf3da4384790a
Ghani Rafif <ghani.rafif.2112@gmail.com>
2021-03-03 00:22:54 +0700

    Add windows support

--- lua/clipboard-image/paste.lua
+++ lua/clipboard-image/paste.lua
@@ -3,16 +3,28 @@ local cmd = vim.cmd
 local fn = vim.fn
 
 -- Check OS and display server
-local cmd_check = ''  -- Var to store command to check clipboard content's type
-local cmd_paste = ''  -- Var to store command to paste clipboard content
-if package.config:sub(1,1) == '/' then
+-- https://vi.stackexchange.com/a/2577/33116
+local get_os = function ()
+  if fn.has('win32') == 1 then
+    return 'Windows'
+  else
+    return tostring(io.popen('uname'):read())
+  end
+end
+
+local cmd_check, cmd_paste = '', ''
+if get_os() == 'Linux' then
   if os.getenv('XDG_SESSION_TYPE') == 'x11' then
     cmd_check = 'xclip -selection clipboard -o -t TARGETS'
-    cmd_paste = 'xclip -selection clipboard -t image/png -o >'
+    cmd_paste = 'xclip -selection clipboard -t image/png -o > %s'
   elseif os.getenv('XDG_SESSION_TYPE') == 'wayland' then
     cmd_check = 'wl-paste --list-types'
-    cmd_paste = 'wl-paste --no-newline --type image/png >'
+    cmd_paste = 'wl-paste --no-newline --type image/png > %s'
   end
+elseif get_os() == 'Windows' then
+  cmd_check = 'Get-Clipboard -Format Image'
+  cmd_paste = '$content = '..cmd_check..';$content.Save(%s, \'png\')'
+  cmd_paste = 'powershell.exe -sta \"'..cmd_paste..'\"'
 end
 
 -- Function that return clipboard content's type
@@ -29,8 +41,11 @@ end
 
 -- Function to check wether clipboard content is image or not
 local is_clipboard_img = function ()
-  if package.config:sub(1,1) == '/' and
+  if get_os() == 'Linux' and
       vim.tbl_contains(get_clipboard_type(), 'image/png') then
+    return true
+  elseif get_os() == 'Windows' and
+    (get_clipboard_type() ~= nil or get_clipboard_type() ~= '') then
     return true
   else
     return false
@@ -47,12 +62,16 @@ end
 
 -- Function that paste image to *path*
 local paste_img_to = function (path)
-  os.execute(cmd_paste..path)
+  os.execute(string.format(cmd_paste, path))
 end
 
 -- Create image's path from dir and img_name
 local img_path = function (dir, img)
-  return dir..'/'..img..'.png'
+  if get_os() == 'Windows' then
+    return dir..'\\'..img..'.png'
+  else
+    return dir..'/'..img..'.png'
+  end
 end
 
 M.paste_img = function ()
