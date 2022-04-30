c39a0ca64643d7e21524ec911959e1c08e170b46
Ekickx <ghani.rafif.2112@gmail.com>
2022-04-30 14:45:22 +0700

    feat: add `:checkhealth` support (#32)
    Now you can do `:checkhealth clipboard-image` to check missing dependencies.

---
+++ lua/clipboard-image/health.lua
@@ -0,0 +1,58 @@
+local M = {}
+local utils = require "clipboard-image.utils"
+local health = require "health"
+
+local packages = {
+  x11 = { name = "xclip", binary = "xclip" },
+  tty = { name = "xclip", binary = "xclip" },
+  wayland = { name = "wl-clipboard", binary = "wl-paste" },
+  darwin = { name = "pngpaste", binary = "pngpaste" },
+  windows = { name = "powershell", binary = "powershell.exe" },
+}
+
+local get_platform = function()
+  local this_os = utils.get_os()
+  if this_os == "Linux" then
+    local display_server = os.getenv "XDG_SESSION_TYPE"
+    return display_server
+  end
+  return this_os:lower()
+end
+
+local check_package_installed = function(package)
+  local name, binary = package.name, package.binary
+  if vim.fn.executable(binary) == 1 then
+    return true, name .. " is installed"
+  end
+  return false, name .. " is not installed"
+end
+
+---Check external dependency on current platform
+---@return boolean is_installed
+---@return string msg message wether dependencies are installed or not
+M.check_current_dep = function()
+  local platform = get_platform()
+  local package = packages[platform]
+
+  if package == nil then
+    return false, "platform is not supported"
+  end
+
+  local is_installed, msg = check_package_installed(package)
+  return is_installed, msg
+end
+
+---Used for `:checkhealth`
+---See also `:h health-lua`
+M.check = function()
+  local is_dep_exist, report_msg = M.check_current_dep()
+
+  health.report_start "Checking dependencies"
+  if is_dep_exist then
+    health.report_ok(report_msg)
+  else
+    health.report_error(report_msg)
+  end
+end
+
+return M
--- lua/clipboard-image/paste.lua
+++ lua/clipboard-image/paste.lua
@@ -1,6 +1,7 @@
 local M = {}
 local conf_utils = require "clipboard-image.config"
 local utils = require "clipboard-image.utils"
+local check_dependency = require("clipboard-image.health").check_current_dep
 local cmd_check, cmd_paste = utils.get_clip_command()
 
 local paste_img_to = function(path)
@@ -8,6 +9,12 @@ local paste_img_to = function(path)
 end
 
 M.paste_img = function(opts)
+  local is_dep_exist, deps_msg = check_dependency()
+  if not is_dep_exist then
+    vim.notify(deps_msg, vim.log.levels.ERROR)
+    return false
+  end
+
   local content = utils.get_clip_content(cmd_check)
   if utils.is_clipboard_img(content) ~= true then
     vim.notify("There is no image data in clipboard", vim.log.levels.ERROR)
